cccc26b3a19a600a1b7900fd54e56e7fdf6e3b3a
Nhitro <julien53laval@gmail.com>
2019-01-10 17:53:58 +0100

    Avancement creerSoiree
    Ajout de deux plugins : Device et DatePicker
    Suppression du répertoire jquery

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,11 +1,11 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "cordova-sqlite-storage.SQLitePlugin",
-    "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
-    "pluginId": "cordova-sqlite-storage",
+    "id": "cordova-plugin-datepicker.DatePicker",
+    "file": "plugins/cordova-plugin-datepicker/www/android/DatePicker.js",
+    "pluginId": "cordova-plugin-datepicker",
     "clobbers": [
-      "SQLitePlugin"
+      "datePicker"
     ]
   },
   {
@@ -23,14 +23,32 @@ module.exports = [
     "merges": [
       "navigator.notification"
     ]
+  },
+  {
+    "id": "cordova-sqlite-storage.SQLitePlugin",
+    "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+    "pluginId": "cordova-sqlite-storage",
+    "clobbers": [
+      "SQLitePlugin"
+    ]
+  },
+  {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+  "cordova-plugin-datepicker": "0.9.3",
+  "cordova-plugin-dialogs": "2.0.1",
   "cordova-plugin-whitelist": "1.3.3",
   "cordova-sqlite-storage": "2.6.0",
-  "cordova-plugin-dialogs": "2.0.1"
+  "cordova-plugin-device": "2.0.2"
 };
 // BOTTOM OF METADATA
 });
