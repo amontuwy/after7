2fa097ba51ec957c105c390e83d7e920452b3355
Damienguyen <damien-nguyen@hotmail.fr>
2019-01-11 15:00:23 +0100

    Ajout du plugin Géoloc

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -9,6 +9,14 @@ module.exports = [
     ]
   },
   {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  },
+  {
     "id": "cordova-plugin-dialogs.notification",
     "file": "plugins/cordova-plugin-dialogs/www/notification.js",
     "pluginId": "cordova-plugin-dialogs",
@@ -25,19 +33,25 @@ module.exports = [
     ]
   },
   {
+    "id": "cordova-plugin-geolocation.geolocation",
+    "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+    "pluginId": "cordova-plugin-geolocation",
+    "clobbers": [
+      "navigator.geolocation"
+    ]
+  },
+  {
+    "id": "cordova-plugin-geolocation.PositionError",
+    "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+    "pluginId": "cordova-plugin-geolocation",
+    "runs": true
+  },
+  {
     "id": "cordova-sqlite-storage.SQLitePlugin",
     "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
     "pluginId": "cordova-sqlite-storage",
     "clobbers": [
       "SQLitePlugin"
-    ]
-  },
-  {
-    "id": "cordova-plugin-device.device",
-    "file": "plugins/cordova-plugin-device/www/device.js",
-    "pluginId": "cordova-plugin-device",
-    "clobbers": [
-      "device"
     ]
   }
 ];
@@ -45,10 +59,11 @@ module.exports.metadata =
 // TOP OF METADATA
 {
   "cordova-plugin-datepicker": "0.9.3",
+  "cordova-plugin-device": "2.0.2",
   "cordova-plugin-dialogs": "2.0.1",
+  "cordova-plugin-geolocation": "4.0.1",
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-sqlite-storage": "2.6.0",
-  "cordova-plugin-device": "2.0.2"
+  "cordova-sqlite-storage": "2.6.0"
 };
 // BOTTOM OF METADATA
 });
--- platforms/android/app/src/main/assets/www/js/app.js
+++ platforms/android/app/src/main/assets/www/js/app.js
@@ -49,12 +49,8 @@ function addRecordSoiree() {
   });
 }
 
-// function goToMap() {
-//    window.location = "map.html";
-// }
-
 function gotoCreateSoiree() {
-  hide(map);
+  hide(mapviz);
   show(creersoiree);
  }
 
@@ -81,20 +77,66 @@ function hide(x) {
 }
 
 function show(x){
+  if (x===mapviz){
+    initMap();
+  }else{
   x.style.display = "block";
+  }
+}
+
+function initMap() {
+    // The location of Moncuq
+    var Moncuq = {lat: 44.338039, lng: 1.207356};
+    // The location of Rapaillou
+    var Rapaillou = {lat: 44.338027, lng: 1.206919};
+    // The map, centered at Moncuq
+    var contenuInfoBulle = "Rapaillou" ;
+    // var map = new google.maps.Map(
+    //     document.getElementById('map'), {zoom: 13, center: Moncuq});
+    var map = new google.maps.Map(
+        mapviz, {zoom: 13, center: Moncuq});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: Rapaillou, map: map});                var infoBulle = new google.maps.InfoWindow({
+        content: contenuInfoBulle
+    })
+    google.maps.event.addListener(marker, 'click', function() {
+        infoBulle.open(map, marker);
+    });
+    infoWindow = new google.maps.InfoWindow;
+    if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+  infoWindow.setPosition(pos);
+  infoWindow.setContent('Location found.');
+  infoWindow.open(map);
+  map.setCenter(pos);
+  }, function() {
+  handleLocationError(true, infoWindow, map.getCenter());
+  });
+  } else {
+  // Browser doesn't support Geolocation
+  handleLocationError(false, infoWindow, map.getCenter());
+  }
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                  'Error: The Geolocation service failed.' :
+                  'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+  }
 }
 
 document.addEventListener('deviceready', function() {
   var login = document.getElementById("login");
-  var map = document.getElementById("map");
+  var mapviz = document.getElementById("map");
   var connected = document.getElementById("connected");
   var creersoiree = document.getElementById("creersoiree");
   $('#creercompte').click(addRecordUser);
   $('#seconnecter').click(verify);
   $('#addsoiree').click(gotoCreateSoiree);
-  // hide(map);
-  // hide(connected);
-  // hide(creersoiree);
   initDatabase();
 });
 
