40960a6507942fe6ebb5dc128119521c224b4be8
Montuwy Angelique <angelique.montuwy@etudiant.univ-rennes1.fr>
2019-02-01 09:45:28 +0100

    the end of the OCI

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,8 @@ var isBrowser = false;
 var latsoiree;
 var lngsoiree;
 
+var searchinprogress = false;
+
 var currentDateFormated = new Date().toLocaleDateString("en-GB"); // DD-MM-YYYY
 
 function initialization() {
@@ -48,6 +50,7 @@ function onDeviceReady() {
 }
 
 function initDatabase() {
+  searchinprogress=false
   database = window.sqlitePlugin.openDatabase({ name: 'after7.db', location: 'default' });
 
   database.transaction(function (transaction) {
@@ -66,6 +69,7 @@ function initDatabase() {
   $("#creersoiree").hide();
   $("#comeback").hide();
   $("#connected").hide();
+  $("#displaysoiree").hide();
 }
 
 function showMessage(message) {
@@ -142,7 +146,7 @@ function addRecordSoiree() {
       $("#map").show();
       $("#search").show();
       $("#creersoiree").hide();
-      $('#comeback').hide()
+      $('#comeback').hide();
     });
   }
 }
@@ -152,7 +156,7 @@ function gotoCreateSoiree() {
   $("#search").hide();
   initialization();
   $("#creersoiree").show();
-  $("#comeback").show()
+  $("#comeback").show();
 }
 
 function verifyuserinbase(name,password)
@@ -230,39 +234,154 @@ function verify() {
 
 
 function initMap() {
-  // The location of Rennes
+
   var Rennes = { lat: 48.117180, lng: -1.677770 };
-  // The location of Université
   var Univ = { lat: 48.113981, lng: -1.638361 };
-  // The map, centered at Moncuq
-//  var contenuInfoBulle = "Soirée ici";
 
   map = new google.maps.Map(
     document.getElementById('map'), { zoom: 12, center: Rennes });
 
-  database.transaction(function (transaction)
-       {transaction.executeSql('SELECT * FROM events',
-          [],
-          function(transaction,results)
-            { var taille = results.rows.length;
-              //populateresults(results);
-              for (let i =0;i<taille;i++){
-                console.log('latitude : '+results.rows.item(i)['geocodlat']+' longitude : '+results.rows.item(i)['geocodlng']);
-                var possoiree = new google.maps.LatLng(results.rows.item(i)['geocodlat'], results.rows.item(i)['geocodlng']);
-                var marker = new google.maps.Marker({ position: possoiree, map: map }); 
-                var infoBulle = new google.maps.InfoWindow();
-                var content = "<p> Titre : "+results.rows.item(i)['titre']+"<br/>"+" Date : "+results.rows.item(i)['date']+"<br/><button onclick='displaySoiree(results.rows.item(i)['titre'], results.rows.item(i)['date'], results.rows.item(i)['lieu'], results.rows.item(i)['descr'], results.rows.item(i)['prix'], results.rows.item(i)['status'])'>En savoir plus</button></p>"
+    if (searchinprogress === false){
+      database.transaction(function (transaction)
+          {transaction.executeSql('SELECT * FROM events',
+              [],
+              function(transaction,results)
+                { var taille = results.rows.length;
 
-                google.maps.event.addListener(marker,'click', (function(marker,content,infoBulle){ 
-                      return function() {
-                          infoBulle.setContent(content);
-                          infoBulle.open(map,marker);
-                      };
-                  })(marker,content,infoBulle));      
+                  for (let i =0;i<taille;i++){
+                    console.log('latitude : '+results.rows.item(i)['geocodlat']+' longitude : '+results.rows.item(i)['geocodlng']);
+                    var possoiree = new google.maps.LatLng(results.rows.item(i)['geocodlat'], results.rows.item(i)['geocodlng']);
+                    var marker = new google.maps.Marker({ position: possoiree, map: map }); 
+                    var infoBulle = new google.maps.InfoWindow();
+                    var tabinfo = [results.rows.item(i)['titre'], results.rows.item(i)['date'], results.rows.item(i)['lieu'], results.rows.item(i)['descr'], results.rows.item(i)['prix'], results.rows.item(i)['statut']]
+                    var content = "<p> Titre : "+results.rows.item(i)['titre']+"<br/> Date : "+results.rows.item(i)['date']+"<br/> <b>Click here for more information</b>"
+
+                    google.maps.event.addListener(marker,'click', (function(marker,content,infoBulle, tabinfo){ 
+                          return function() {
+                              var div = document.createElement('div');
+                              div.innerHTML = content;
+                              div.onclick = function(){displaySoiree(tabinfo)};
+                              infoBulle.setContent(div);
+                              infoBulle.open(map,marker);
+                          };
+                      })(marker,content,infoBulle, tabinfo));      
+                  }
+                },errorCB,successCB
+          );});
+     } else 
+     { //s'il y a une recherche en cours
+      var lieuSearch = $('input[id=lieuSearch]').val();
+      var kilometreSearch = $('input[id=distanceSearch]').val();
+      var dateSearch = $('input[id=dateSearch]').val();
+      var typeSearch = $('select[id=statutSearch]').val();
+      var themeSearch = $('select[id=themeSearch]').val();
+      var request = 'SELECT * FROM events ';
+      var condition = '';
+      var param = []
+    
+      if(lieuSearch != null && lieuSearch != ""){
+    
+        if(dateSearch != null && dateSearch != ""){
+          if(condition != ''){
+            condition += ' AND '
+          }
+          condition += 'date=?';
+          param.push(dateSearch)
+        }
+    
+        if(typeSearch != null && typeSearch != "" && typeSearch != "all"){
+          if(condition != ''){
+            condition += ' AND '
+          }
+          condition += 'statut=?';
+          param.push(typeSearch)
+        }
+    
+        if(themeSearch != null && themeSearch != "" && themeSearch != 0){
+          if(condition != ''){
+            condition += ' AND '
+          }
+          condition += 'theme=?';
+          param.push(themeSearch)
+        }
+    
+        if(condition != ''){
+          request = request + 'WHERE ' + condition;
+        }
+    
+        if(kilometreSearch != null && kilometreSearch != "0"){
+          nativegeocoder.forwardGeocode(success, failure, lieuSearch, { useLocale: true, maxResults: 1 });
+    
+          function success(coordinates) {
+            latsoiree = coordinates[0].latitude;
+            lngsoiree = coordinates[0].longitude;
+            database.transaction(function (transaction){
+              transaction.executeSql(request, param, function(transaction,results){
+                var len = results.rows.length;
+                if(len == 0){
+                  showMessage("Pas de soirées pour votre recherche");
+                }
+                else {
+                  for(i = 0; i < results.rows.length; i++){
+                    var lat = results.rows.item(i)["geocodlat"];
+                    var long = results.rows.item(i)["geocodlng"];
+                    var km = distanceInKmBetweenEarthCoordinates(latsoiree, lngsoiree, lat, long);
+                    if(km <= kilometreSearch){
+                      var possoiree = new google.maps.LatLng(results.rows.item(i)['geocodlat'], results.rows.item(i)['geocodlng']);
+                      var marker = new google.maps.Marker({ position: possoiree, map: map }); 
+                      var infoBulle = new google.maps.InfoWindow();
+                      var tabinfo = [results.rows.item(i)['titre'], results.rows.item(i)['date'], results.rows.item(i)['lieu'], results.rows.item(i)['descr'], results.rows.item(i)['prix'], results.rows.item(i)['statut']]
+                      var content = "<p> Titre : "+results.rows.item(i)['titre']+"<br/> Date : "+results.rows.item(i)['date']+"<br/> <b>Click here for more information</b>"
+  
+                      google.maps.event.addListener(marker,'click', (function(marker,content,infoBulle, tabinfo){ 
+                            return function() {
+                                var div = document.createElement('div');
+                                div.innerHTML = content;
+                                div.onclick = function(){displaySoiree(tabinfo)};
+                                infoBulle.setContent(div);
+                                infoBulle.open(map,marker);
+                            };
+                        })(marker,content,infoBulle, tabinfo));    
+                    }
+                  }
+               }
+             }, errorCB);
+           }, errorCB,successCB);
+         }
+         function failure(err) {
+           showMessage(err);
+         }
+        } else {
+          database.transaction(function (transaction){
+            transaction.executeSql(request, param, function(transaction,results){
+              var len = results.rows.length;
+              if(len == 0){
+                showMessage("Erreur : Pas de soirées");
               }
-            },errorCB,successCB
-      );});
- 
+              else {
+                for(i = 0; i < results.rows.length; i++){
+                  var possoiree = new google.maps.LatLng(results.rows.item(i)['geocodlat'], results.rows.item(i)['geocodlng']);
+                      var marker = new google.maps.Marker({ position: possoiree, map: map }); 
+                      var infoBulle = new google.maps.InfoWindow();
+                      var tabinfo = [results.rows.item(i)['titre'], results.rows.item(i)['date'], results.rows.item(i)['lieu'], results.rows.item(i)['descr'], results.rows.item(i)['prix'], results.rows.item(i)['statut']]
+                      var content = "<p> Titre : "+results.rows.item(i)['titre']+"<br/> Date : "+results.rows.item(i)['date']+"<br/> <b>Click here for more information</b>"
+  
+                      google.maps.event.addListener(marker,'click', (function(marker,content,infoBulle, tabinfo){ 
+                            return function() {
+                                var div = document.createElement('div');
+                                div.innerHTML = content;
+                                div.onclick = function(){displaySoiree(tabinfo)};
+                                infoBulle.setContent(div);
+                                infoBulle.open(map,marker);
+                            };
+                        })(marker,content,infoBulle, tabinfo));    
+                }
+             }
+           }, errorCB);
+         }, errorCB,successCB);
+      }
+    }
+  }
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
        var pos = {
@@ -294,8 +413,21 @@ function validTitre(value){
   }
  }
 
-function displaySoiree(titre, date, lieu, descr, prix, status){
-   
+function displaySoiree(tab){
+   $("#map").hide();
+   $("#search").hide();
+   $("#comeback").show();
+   $("#displaysoiree").show();
+   $("#soireeTitre").text(tab[0]);
+   $("#soireeDate").text(tab[1]);
+   $("#soireeLieu").text(tab[2])
+   $("#soireeDescr").text(tab[3]);
+   $("#soireePrix").text(tab[4] + "€");
+   if (tab[5]==="on"){
+      $("#soireeType").text("Soirée privée");
+   } else{
+      $("#soireeType").text("Soirée publique");
+   }
 }
  
  function validLieu(value){
@@ -335,7 +467,8 @@ function retour() {
   $("#map").show();
   $("#search").show();
   $("#creersoiree").hide();
-  $("#comeback").hide()
+  $("#comeback").hide();
+  $("#displaysoiree").hide();
  }
 
 function geocoding(adresse){
@@ -351,13 +484,87 @@ function geocoding(adresse){
   }
 }
 
+ function gotoSearch() {
+   searchinprogress=true;
+   initMap();
+ }
+
+
+function degreesToRadians(degrees) {
+  return degrees * Math.PI / 180;
+}
+
+function geocentering(){
+
+  function success(result) {
+    var firstResult = JSON.stringify(result[0]);
+    $('input[id=lieuSearch]').val(result[0]['subThoroughfare'] +" "+ result[0]['thoroughfare'] +" "+ result[0]['locality']);
+  }
+
+  function failure(err) {
+   console.log(err);
+  }
+
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  }
+
+  var pos;
+
+  if (navigator.geolocation) {
+   navigator.geolocation.getCurrentPosition(
+     function (position) {
+       pos = {
+         lat: position.coords.latitude,
+         lng: position.coords.longitude
+       };
+       //infoWindow.setPosition(pos);
+       //infoWindow.setContent('Vous êtes ici.');
+       //infoWindow.open(map);
+       //map.setCenter(pos);
+       nativegeocoder.reverseGeocode(success, failure, pos.lat , pos.lng , { useLocale: true, maxResults: 1 });
+     }, 
+     function () {
+       handleLocationError(true, infoWindow, map.getCenter());
+     }
+   );
+  }
+
+  else {
+   // Browser doesn't support Geolocation
+   handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
+  var earthRadiusKm = 6371;
+
+  var dLat = degreesToRadians(lat2-lat1);
+  var dLon = degreesToRadians(lon2-lon1);
+
+  lat1 = degreesToRadians(lat1);
+  lat2 = degreesToRadians(lat2);
+
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  return earthRadiusKm * c;
+}
+
+function inscriptionvalidee(){
+  showMessage("Votre inscription a bien été enregistrée")
+}
+
 document.addEventListener('deviceready', function () {
   $('#creercompte').click(addRecordUser);
   $('#seconnecter').click(verify);
   $('#addsoiree').click(gotoCreateSoiree);
   $('#creerSoireeinDB').click(addRecordSoiree);
-  $('#back').click(retour)
+  $('#back').click(retour);
+  $('#searchsoiree').click(gotoSearch);
+  $('#locationsearch').click(geocentering);
+  $('#inscription').click(inscriptionvalidee);
   initDatabase();
 });
-
-
